f468174d75d70d80fbe374965e24e6dbf3db2877
ABRAR <abrarrafi174@gmail.com>
2023-02-22 12:09:41 +0600

    Frontend: Rating Star Design

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Card } from 'react-bootstrap';
+import Rating from './Rating';
+
 
 function Product({ product }){
     return(
@@ -17,7 +19,8 @@ function Product({ product }){
                     </a>
                     <Card.Text as='div'>
                         <div className="my-3">
-                            {product.rating} From {product.numReviews} Reviews 
+                            <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+
                         </div>
                     </Card.Text>
 
